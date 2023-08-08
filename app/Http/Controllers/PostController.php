2c51952ebfaff22e3a942245d1948d8c7e846475
yukito0315 <ciyk20114@g.nihon-u.ac.jp>
2023-08-08 20:55:18 +0900

    a

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 
-class Postcontroller extends Controller
+class PostController extends Controller
 {
     public function index(Post $post)
     {
