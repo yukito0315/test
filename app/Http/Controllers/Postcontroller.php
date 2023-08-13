25cbbd1075976bf9c8fee335b58b31d19fed6a4d
yukito0315 <ciyk20114@g.nihon-u.ac.jp>
2023-08-13 20:46:14 +0900

    エラー中

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -9,6 +9,6 @@ class Postcontroller extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts'=>$post->get()]);
+        return view('posts/index')->with(['posts'=>$post->getPaginateByLimit(1)]);
     }
 }
