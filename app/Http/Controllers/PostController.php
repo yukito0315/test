41698f432ed02c74afb835d1b4eb0846809c7f9e
yukito0315 <ciyk20114@g.nihon-u.ac.jp>
2023-09-16 02:11:46 +0900

    削除機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,4 +36,10 @@ class PostController extends Controller
         $post->fill($input_post)->save();
         return redirect('/posts/' . $post->id);
     }
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
+        
+    }
 }
